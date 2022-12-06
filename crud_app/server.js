b875922bcfec3feb59f1c55515cd403d49615371
Quang Huy <quanghuy3014@gmail.com>
2022-12-06 16:55:37 +0700

    CRUD APP Router and Database Connection

--- crud_app/server.js
+++ crud_app/server.js
@@ -2,17 +2,30 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
+const path = require('path');
+const connectDB = require('./server/database/connection');
 const app = express();
 
 dotenv.config({ path: './config.env' });
 const PORT = process.env.PORT || 8080;
 
 app.use(morgan('tiny'));
+//connect to mongodb
+connectDB();
+
+// parse application/x-www-form-urlencoded
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
-app.get('/', (req, res) => {
-    res.send("CRUD APP");
-})
+
+//set view engine
+app.set("view engine", "ejs");
+//load assets
+app.use('/css', express.static(path.resolve(__dirname, "assets/css")));
+app.use('/img', express.static(path.resolve(__dirname, "assets/img")));
+app.use('/js', express.static(path.resolve(__dirname, "assets/js")));
+
+//routes
+app.use('/', require('./server/routes/router'));
 
 
 app.listen(PORT, () => {
